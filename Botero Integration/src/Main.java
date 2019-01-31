a43ef7417b1f016677dee7ea8f2074702ced2177
marianaboteroq <46663848+marianaboteroq@users.noreply.github.com>
2019-01-31 15:42:30 -0500

    Initial Commit

---
+++ Botero Integration/src/Main.java
@@ -0,0 +1,63 @@
+
+public class Main {
+// Mariana Botero //
+// A program integrating skills learned in COP 2006
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+		/* |^| 'sout' + ctrl + space, or 'sysout' + cmd + space |^| */
+		
+		int int1 = 2;
+		double double1 = 2.0;
+		String string1 = "2";
+		
+		/* Declare and initialize second integer, double, and string variables. */
+		
+		int int2 = 2;
+		double double2 = 2.0;
+		String string2 = "2";
+		
+		/* Print the sum of both integer variables on a new line. */
+        System.out.println("The result of + with ints is " + (int1 + int2));
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println("The result of + with doubles is " + (double1 + double2));
+        /* Concatenate and print the String variables on a new line */
+        System.out.println("The result of + with Strings is " + string1 + string2);
+		
+/*         To change the type of cursor, press the insert key 
+		   Any time you go from numbers to strings, clear out input string by
+		   adding "scan.nextLine(); 											*/
+		 final double SALE_TAX_RATE = 0.65; 
+		 // |^| it can't/ shouldn't be changed after you've initialized it. 
+		 
+		 Scanner scan = new Scanner(System.in);
+	        int a = scan.nextInt();
+	        int b = scan.nextInt();
+	        int c = scan.nextInt();
+	        scan.close();
+	        // Complete this line
+	        // Complete this line
+
+	        System.out.println(a);
+	        System.out.println(b);
+	        System.out.println(c);
+	        // Complete this line
+	        // Complete this line
+	        
+	        
+	            Scanner scan = new Scanner(System.in);
+	            int i = scan.nextInt();
+	            double d = scan.nextDouble();
+	            String f = scan.nextLine();
+	            String s = scan.nextLine();
+	                 
+
+	            // Write your code here.
+
+	            System.out.println("String: " + s);
+	            System.out.println("Double: " + d);
+	            System.out.println("Int: " + i);
+		 
+		 
+	}
+				}
+
